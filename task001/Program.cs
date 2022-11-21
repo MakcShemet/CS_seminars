084afe737ccb50b7c10f91a897d7b975b2a03df5
DevMasterSh <dev_master_sh@mail.ru>
2022-11-21 11:39:36 +0300

    Changed task001

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("Input number:");
-int n = Convert.ToInt32(Console.ReadLine());
-int sum = n * n;
-Console.WriteLine("n * n = " + sum);
+﻿Console.WriteLine("Input tow number:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int square = a * a;
+if (square == b)
+{
+    Console.WriteLine("Число " + a + "явлется квадратом " + b);
+}
+else {Console.WriteLine("Число " + a + "не явлется квадратом " + b);
+}
